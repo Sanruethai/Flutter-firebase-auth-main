4f1833d18d75bb036208bb7c6d62947fd4c279ca
Sanruethai Saybut <119285424+Sanruethai@users.noreply.github.com>
2025-02-19 21:00:23 +0700

    Initial commit with Flutter-firebase-auth-main directory

---
+++ Flutter-firebase-auth-main/lib/create_account_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CreateAccountScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Create Account")),
+      body: Center(child: Text("Create Account Page")),
+    );
+  }
+}
